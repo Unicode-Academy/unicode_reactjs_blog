69b09ac1c5a91f80a18a4b10951b49560bc2da1e
Hoàng An F8 <hoangan@fullstack.edu.vn>
2024-07-25 15:17:01 +0700

    ReactJS Blog - 05

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -8,18 +8,37 @@ import {
   Typography,
 } from "@mui/material";
 import LinkBehavior from "./LinkBehavior";
+import { Link } from "@mui/material";
 
 export default function CartItem({ id, title, image }) {
   return (
     <Card sx={{ maxWidth: 345 }}>
       <CardMedia sx={{ height: 140 }} image={image} title={title} />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          {title}
+      <CardContent style={{ paddingBottom: 0 }}>
+        <Typography
+          gutterBottom
+          variant="h5"
+          component="div"
+          fontSize={18}
+          style={{ minHeight: "50px" }}
+        >
+          <Link
+            color="inherit"
+            style={{ textDecoration: "none" }}
+            to={`/post/${id}`}
+            component={LinkBehavior}
+          >
+            {title}
+          </Link>
         </Typography>
       </CardContent>
       <CardActions>
-        <Button size="small" component={LinkBehavior} to={`/post/${id}`}>
+        <Button
+          variant="outlined"
+          size="small"
+          component={LinkBehavior}
+          to={`/post/${id}`}
+        >
           Xem chi tiết
         </Button>
       </CardActions>
