978b8bceaf27575514d70e556e366a08846ab531
Hoàng An F8 <hoangan@fullstack.edu.vn>
2024-07-27 14:30:27 +0700

    ReactJS Blog - 11

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,12 +1,14 @@
 import Backdrop from "@mui/material/Backdrop";
 import CircularProgress from "@mui/material/CircularProgress";
-export default function Loading() {
-  return (
+export default function Loading({ type = "backdrop" }) {
+  return type === "backdrop" ? (
     <Backdrop
       sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
       open={true}
     >
       <CircularProgress color="inherit" />
     </Backdrop>
+  ) : (
+    <CircularProgress />
   );
 }
