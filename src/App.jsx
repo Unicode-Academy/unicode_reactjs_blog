f05d56f05cfec8e4b293744465fb8611fad12da8
Hoàng An F8 <hoangan@fullstack.edu.vn>
2024-07-27 17:34:26 +0700

    ReactJS Blog - 14

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home/Home";
 import MainLayout from "./layouts/MainLayout";
 import Post from "./pages/Post/Post";
+import Author from "./pages/Post/Author";
 
 export default function App() {
   return (
@@ -9,6 +10,7 @@ export default function App() {
       <Route element={<MainLayout />}>
         <Route path="/" element={<Home />} />
         <Route path="/post/:id" element={<Post />} />
+        <Route path="/author/:id" element={<Author />} />
       </Route>
     </Routes>
   );
